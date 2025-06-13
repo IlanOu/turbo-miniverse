7880dd02fa16eb34460752b5ae021aa19025e79c
IlanOu <71207643+IlanOu@users.noreply.github.com>
2025-06-13 16:28:48 +0200

    feat: add some juice

--- Assets/Scripts/Menu/GarageUI.cs
+++ Assets/Scripts/Menu/GarageUI.cs
@@ -3,6 +3,9 @@ using Car;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
+using UnityEngine.EventSystems;
 
 namespace Menu
 {
@@ -20,6 +23,16 @@ namespace Menu
         [SerializeField] private Button actionButton;
         [SerializeField] private TextMeshProUGUI actionButtonText;
         [SerializeField] private Image carPreviewImage;
+        [SerializeField] private RectTransform carInfoPanel;
+        [SerializeField] private CanvasGroup mainCanvasGroup;
+        
+        [Header("Animation Settings")]
+        [SerializeField] private float openAnimDuration = 0.5f;
+        [SerializeField] private float switchCarAnimDuration = 0.3f;
+        [SerializeField] private float buttonClickAnimDuration = 0.2f;
+        [SerializeField] private float buyAnimDuration = 0.5f;
+        [SerializeField] private Ease openEase = Ease.OutBack;
+        [SerializeField] private Ease switchEase = Ease.InOutQuad;
         
         [Header("Preview Settings")]
         [SerializeField] private Color unlockedColor = Color.white;
@@ -31,6 +44,7 @@ namespace Menu
         private int currentCarIndex = 0;
         private int selectedCarIndex = 0;
         private MoneyManager moneyManager;
+        private bool isAnimating = false;
         
         // Variable pour contrôler si l'UI doit se fermer automatiquement
         private bool shouldCloseUI = false;
@@ -52,15 +66,30 @@ namespace Menu
         {
             moneyManager = MoneyManager.Instance;
             
-            // Configuration des boutons
+            // Configuration des boutons avec animations
             if (previousButton != null)
-                previousButton.onClick.AddListener(PreviousCar);
+            {
+                previousButton.onClick.AddListener(() => {
+                    AnimateButtonClick(previousButton.transform);
+                    PreviousCar();
+                });
+            }
                 
             if (nextButton != null)
-                nextButton.onClick.AddListener(NextCar);
+            {
+                nextButton.onClick.AddListener(() => {
+                    AnimateButtonClick(nextButton.transform);
+                    NextCar();
+                });
+            }
                 
             if (actionButton != null)
-                actionButton.onClick.AddListener(OnActionButtonClick);
+            {
+                actionButton.onClick.AddListener(() => {
+                    AnimateButtonClick(actionButton.transform);
+                    OnActionButtonClick();
+                });
+            }
             
             // La première voiture est possédée par défaut
             if (carsData.Count > 0)
@@ -78,24 +107,86 @@ namespace Menu
             
             // Désactiver l'UI au démarrage
             gameObject.SetActive(false);
+            
+            // Initialiser DOTween si nécessaire
+            DOTween.Init();
+        }
+        
+        private void AnimateButtonClick(Transform buttonTransform)
+        {
+            // Séquence d'animation pour le clic de bouton
+            Sequence clickSequence = DOTween.Sequence();
+            clickSequence.Append(buttonTransform.DOScale(0.9f, buttonClickAnimDuration / 2));
+            clickSequence.Append(buttonTransform.DOScale(1f, buttonClickAnimDuration / 2));
         }
         
         public void NextCar()
         {
+            if (isAnimating) return;
+            
             currentCarIndex++;
             if (currentCarIndex >= carsData.Count)
                 currentCarIndex = 0;
                 
-            UpdateCarDisplay();
+            AnimateCarSwitch(true);
         }
         
         public void PreviousCar()
         {
+            if (isAnimating) return;
+            
             currentCarIndex--;
             if (currentCarIndex < 0)
                 currentCarIndex = carsData.Count - 1;
                 
-            UpdateCarDisplay();
+            AnimateCarSwitch(false);
+        }
+        
+        private void AnimateCarSwitch(bool isNext)
+        {
+            isAnimating = true;
+            
+            // Animer la sortie des éléments actuels
+            Sequence exitSequence = DOTween.Sequence();
+            
+            // Faire sortir l'image par le côté
+            float xOffset = isNext ? -100f : 100f;
+            exitSequence.Join(carPreviewImage.transform.DOLocalMoveX(xOffset, switchCarAnimDuration / 2).SetEase(switchEase));
+            exitSequence.Join(carPreviewImage.DOFade(0, switchCarAnimDuration / 2));
+            
+            // Faire sortir les textes par le bas
+            if (carNameText != null)
+                exitSequence.Join(carNameText.DOFade(0, switchCarAnimDuration / 2));
+            
+            if (carStatsText != null)
+                exitSequence.Join(carStatsText.DOFade(0, switchCarAnimDuration / 2));
+            
+            // Après la sortie, mettre à jour les données et animer l'entrée
+            exitSequence.OnComplete(() => {
+                UpdateCarDisplay();
+                
+                // Animer l'entrée des nouveaux éléments
+                Sequence enterSequence = DOTween.Sequence();
+                
+                // Positionner l'image de l'autre côté avant l'animation
+                carPreviewImage.transform.localPosition = new Vector3(-xOffset, carPreviewImage.transform.localPosition.y, 0);
+                carPreviewImage.color = new Color(carPreviewImage.color.r, carPreviewImage.color.g, carPreviewImage.color.b, 0);
+                
+                // Faire entrer l'image
+                enterSequence.Join(carPreviewImage.transform.DOLocalMoveX(0, switchCarAnimDuration / 2).SetEase(switchEase));
+                enterSequence.Join(carPreviewImage.DOFade(1, switchCarAnimDuration / 2));
+                
+                // Faire entrer les textes
+                if (carNameText != null)
+                    enterSequence.Join(carNameText.DOFade(1, switchCarAnimDuration / 2));
+                
+                if (carStatsText != null)
+                    enterSequence.Join(carStatsText.DOFade(1, switchCarAnimDuration / 2));
+                
+                enterSequence.OnComplete(() => {
+                    isAnimating = false;
+                });
+            });
         }
         
         private void UpdateCarDisplay()
@@ -115,7 +206,8 @@ namespace Menu
                     carPreviewImage.enabled = true;
                     
                     // Appliquer la couleur en fonction du statut de déverrouillage
-                    carPreviewImage.color = data.owned ? unlockedColor : lockedColor;
+                    Color targetColor = data.owned ? unlockedColor : lockedColor;
+                    carPreviewImage.color = new Color(targetColor.r, targetColor.g, targetColor.b, carPreviewImage.color.a);
                 }
                 else
                 {
@@ -163,6 +255,8 @@ namespace Menu
         
         private void OnActionButtonClick()
         {
+            if (isAnimating) return;
+            
             CarData data = carsData[currentCarIndex];
             
             if (data.owned)
@@ -183,6 +277,12 @@ namespace Menu
             
             if (moneyManager == null || moneyManager.GetMoney() < data.price)
             {
+                // Animation de refus d'achat
+                if (actionButton != null)
+                {
+                    actionButton.transform.DOShakePosition(0.5f, 10, 20, 90, false, true);
+                }
+                
                 Debug.Log("Pas assez d'argent pour acheter cette voiture");
                 return;
             }
@@ -194,8 +294,33 @@ namespace Menu
             
             Debug.Log($"Voiture {data.carName} achetée pour {data.price} pièces");
             
-            // Mettre à jour l'interface après l'achat
-            UpdateCarDisplay(); // Mettre à jour l'affichage complet pour changer la couleur
+            // Animation d'achat réussi
+            AnimatePurchase();
+        }
+        
+        private void AnimatePurchase()
+        {
+            isAnimating = true;
+            
+            // Séquence d'animation pour l'achat
+            Sequence purchaseSequence = DOTween.Sequence();
+            
+            // Faire tourner l'image
+            if (carPreviewImage != null)
+            {
+                purchaseSequence.Append(carPreviewImage.transform.DOScale(1.2f, buyAnimDuration / 2).SetEase(Ease.OutBack));
+                purchaseSequence.Join(carPreviewImage.transform.DORotate(new Vector3(0, 360, 0), buyAnimDuration, RotateMode.FastBeyond360));
+                purchaseSequence.Append(carPreviewImage.transform.DOScale(1f, buyAnimDuration / 2).SetEase(Ease.OutBack));
+                
+                // Changer la couleur de grisé à normal
+                purchaseSequence.Join(carPreviewImage.DOColor(unlockedColor, buyAnimDuration / 2));
+            }
+            
+            // Mettre à jour l'interface après l'animation
+            purchaseSequence.OnComplete(() => {
+                UpdateActionButton();
+                isAnimating = false;
+            });
         }
         
         private void SelectCar()
@@ -214,12 +339,56 @@ namespace Menu
             
             Debug.Log($"Voiture {carsData[currentCarIndex].carName} sélectionnée et activée");
             
-            // IMPORTANT: Fermer l'UI UNIQUEMENT lors de la sélection
-            shouldCloseUI = true;
-            gameObject.SetActive(false);
+            // Animer la fermeture de l'UI
+            AnimateClose();
         }
         
-        private void PositionSelectedCar()
+        private void AnimateClose()
+        {
+            isAnimating = true;
+            
+            // Séquence d'animation pour la fermeture
+            Sequence closeSequence = DOTween.Sequence();
+            
+            // Faire disparaître progressivement tous les éléments
+            if (mainCanvasGroup != null)
+            {
+                closeSequence.Append(mainCanvasGroup.DOFade(0, openAnimDuration).SetEase(Ease.InBack));
+            }
+            else
+            {
+                // Animer chaque élément individuellement si pas de CanvasGroup
+                if (carPreviewImage != null)
+                    closeSequence.Join(carPreviewImage.DOFade(0, openAnimDuration));
+                
+                if (carNameText != null)
+                    closeSequence.Join(carNameText.DOFade(0, openAnimDuration));
+                
+                if (carStatsText != null)
+                    closeSequence.Join(carStatsText.DOFade(0, openAnimDuration));
+                
+                if (actionButton != null)
+                    closeSequence.Join(actionButton.GetComponent<CanvasGroup>()?.DOFade(0, openAnimDuration));
+                
+                if (previousButton != null)
+                    closeSequence.Join(previousButton.GetComponent<CanvasGroup>()?.DOFade(0, openAnimDuration));
+                
+                if (nextButton != null)
+                    closeSequence.Join(nextButton.GetComponent<CanvasGroup>()?.DOFade(0, openAnimDuration));
+                
+                if (carInfoPanel != null)
+                    closeSequence.Join(carInfoPanel.DOScale(0.8f, openAnimDuration).SetEase(Ease.InBack));
+            }
+            
+            // Désactiver l'UI après l'animation
+            closeSequence.OnComplete(() => {
+                shouldCloseUI = true;
+                gameObject.SetActive(false);
+                isAnimating = false;
+            });
+        }
+        
+                private void PositionSelectedCar()
         {
             if (carSpawnPoint == null || carSelector == null || 
                 selectedCarIndex >= carSelector.cars.Count)
@@ -239,6 +408,11 @@ namespace Menu
                 child.localPosition = Vector3.zero;
                 child.localRotation = Quaternion.identity;
             }
+            
+            // Animation de la voiture sélectionnée
+            selectedCar.transform.DOScale(0, 0.01f).OnComplete(() => {
+                selectedCar.transform.DOScale(1, 0.5f).SetEase(Ease.OutBack);
+            });
         }
         
         private string GenerateStars(int value)
@@ -262,6 +436,88 @@ namespace Menu
             // Afficher la voiture actuellement sélectionnée
             currentCarIndex = selectedCarIndex;
             UpdateCarDisplay();
+            
+            // Animer l'ouverture
+            AnimateOpen();
+        }
+        
+        private void AnimateOpen()
+        {
+            isAnimating = true;
+            
+            // Préparer les éléments pour l'animation
+            if (mainCanvasGroup != null)
+            {
+                mainCanvasGroup.alpha = 0;
+            }
+            else
+            {
+                // Préparer chaque élément individuellement
+                if (carPreviewImage != null)
+                {
+                    carPreviewImage.color = new Color(carPreviewImage.color.r, carPreviewImage.color.g, carPreviewImage.color.b, 0);
+                    carPreviewImage.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+                }
+                
+                if (carNameText != null)
+                    carNameText.alpha = 0;
+                
+                if (carStatsText != null)
+                    carStatsText.alpha = 0;
+                
+                if (actionButton != null && actionButton.GetComponent<CanvasGroup>() != null)
+                    actionButton.GetComponent<CanvasGroup>().alpha = 0;
+                
+                if (previousButton != null && previousButton.GetComponent<CanvasGroup>() != null)
+                    previousButton.GetComponent<CanvasGroup>().alpha = 0;
+                
+                if (nextButton != null && nextButton.GetComponent<CanvasGroup>() != null)
+                    nextButton.GetComponent<CanvasGroup>().alpha = 0;
+                
+                if (carInfoPanel != null)
+                    carInfoPanel.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            }
+            
+            // Séquence d'animation pour l'ouverture
+            Sequence openSequence = DOTween.Sequence();
+            
+            // Animer l'apparition progressive
+            if (mainCanvasGroup != null)
+            {
+                openSequence.Append(mainCanvasGroup.DOFade(1, openAnimDuration).SetEase(openEase));
+            }
+            else
+            {
+                // Animer chaque élément avec un léger décalage
+                if (carInfoPanel != null)
+                    openSequence.Join(carInfoPanel.DOScale(1, openAnimDuration).SetEase(openEase));
+                
+                if (carPreviewImage != null)
+                {
+                    openSequence.Join(carPreviewImage.DOFade(1, openAnimDuration));
+                    openSequence.Join(carPreviewImage.transform.DOScale(1, openAnimDuration).SetEase(openEase));
+                }
+                
+                if (carNameText != null)
+                    openSequence.Join(carNameText.DOFade(1, openAnimDuration).SetDelay(0.1f));
+                
+                if (carStatsText != null)
+                    openSequence.Join(carStatsText.DOFade(1, openAnimDuration).SetDelay(0.2f));
+                
+                if (actionButton != null && actionButton.GetComponent<CanvasGroup>() != null)
+                    openSequence.Join(actionButton.GetComponent<CanvasGroup>().DOFade(1, openAnimDuration).SetDelay(0.3f));
+                
+                if (previousButton != null && previousButton.GetComponent<CanvasGroup>() != null)
+                    openSequence.Join(previousButton.GetComponent<CanvasGroup>().DOFade(1, openAnimDuration).SetDelay(0.3f));
+                
+                if (nextButton != null && nextButton.GetComponent<CanvasGroup>() != null)
+                    openSequence.Join(nextButton.GetComponent<CanvasGroup>().DOFade(1, openAnimDuration).SetDelay(0.3f));
+            }
+            
+            // Terminer l'animation
+            openSequence.OnComplete(() => {
+                isAnimating = false;
+            });
         }
         
         // Méthode pour vérifier si les sprites sont correctement chargés
@@ -278,6 +534,61 @@ namespace Menu
             
             // Forcer la mise à jour de l'affichage
             UpdateCarDisplay();
+        }
+        
+        // Méthode pour ajouter des effets de survol aux boutons
+        private void OnHoverEffects()
+        {
+            // Ajouter des écouteurs d'événements pour les effets de survol
+            if (previousButton != null)
+            {
+                AddHoverEffect(previousButton);
+            }
+            
+            if (nextButton != null)
+            {
+                AddHoverEffect(nextButton);
+            }
+            
+            if (actionButton != null)
+            {
+                AddHoverEffect(actionButton);
+            }
+        }
+        
+        private void AddHoverEffect(Button button)
+        {
+            // Ajouter des écouteurs d'événements pour le survol
+            EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>() ?? button.gameObject.AddComponent<EventTrigger>();
+            
+            // Entrée du survol
+            EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+            entryEnter.eventID = EventTriggerType.PointerEnter;
+            entryEnter.callback.AddListener((data) => {
+                button.transform.DOScale(1.1f, 0.2f).SetEase(Ease.OutQuad);
+            });
+            trigger.triggers.Add(entryEnter);
+            
+            // Sortie du survol
+            EventTrigger.Entry entryExit = new EventTrigger.Entry();
+            entryExit.eventID = EventTriggerType.PointerExit;
+            entryExit.callback.AddListener((data) => {
+                button.transform.DOScale(1f, 0.2f).SetEase(Ease.OutQuad);
+            });
+            trigger.triggers.Add(entryExit);
+        }
+        
+        // Méthode pour nettoyer les tweens à la destruction
+        private void OnDestroy()
+        {
+            DOTween.Kill(carPreviewImage);
+            DOTween.Kill(carNameText);
+            DOTween.Kill(carStatsText);
+            DOTween.Kill(actionButton);
+            DOTween.Kill(previousButton);
+            DOTween.Kill(nextButton);
+            DOTween.Kill(carInfoPanel);
+            DOTween.Kill(mainCanvasGroup);
         }
     }
 }
